37c467124265afc3d03421674d8316668ca6f28d
Valentina Corak <valentinacorak@gmail.com>
2021-05-10 18:22:47 +0200

    Finnaly five days forecast scripy is working but not displaying now...

--- script.js
+++ script.js
@@ -11,24 +11,44 @@ form.addEventListener("submit", search);
 
 searchCity("Antwerp");
 
+// Display responses in html
 function cityWeather(response) {
     let city = document.querySelector("#city");
     city.innerHTML = response.data.name;
+    let temperature = document.querySelector("#main-temp");
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+    let mainIcon = document.querySelector("#weather-icon");
+    mainIcon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    mainIcon.setAttribute("alt", response.data.weather[0].description);
+
 }
 
 // Displaying 5 day weather forecast
-
-function displayFiveDayForecast(response) {
-    let forecastReport = document.querySelector("#forecast-report");
-    forecastReport.innerHTML = null;
+function displayFiveDaysForecast(response) {
+    let forecastData = document.querySelector("#forecast-report");
+    forecastData.innerHTML = null;
     let forecast = null;
 
-    for (let index = 0; index < 6; index++) {
+    for (let index = 0; index < 5; index++) {
         forecast = response.data.list[index];
-        forecastReport.innerHTML += `<div class ="col-2>
-<h3>`
+        forecastData.innerHTMl += `<div class="col-2">
+              <img
+                src="http://openweathermap.org/img/wn/${
+            forecast.weather[0].icon
+        }@2x.png"
+                alt=""
+              />
+              <div class="hourly-forecast-temperature">
+                <strong>${Math.round(
+            forecast.main.temp_max
+        )}°</strong> ${Math.round(forecast.main.temp_min)}°
+              </div>
+        </div>`;
     }
 }
+
 
 
 // Get axios
@@ -38,7 +58,8 @@ function searchCity(city) {
     axios.get(apiUrl).then(cityWeather);
 
     apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayFiveDayForecast);
+    axios.get(apiUrl).then(displayFiveDaysForecast);
 
 }
+
 
