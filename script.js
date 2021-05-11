3fe1ca70bffb7324a1bc71a77b2e8a18ec94c37e
Valentina Corak <valentinacorak@gmail.com>
2021-05-11 12:31:38 +0200

    Trying to display 5 days, it is ok, but it doesn't show different five days...

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+
+// Get axios
+function searchCity(city) {
+    let apiKey = "7f2644b98089db65deba1530820c54a1";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    axios.get(apiUrl).then(cityWeather);
+
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayFiveDaysForecast);
+
+
+
+}
+
 // Search event city weather
 function search(event) {
     event.preventDefault();
@@ -11,25 +25,6 @@ form.addEventListener("submit", search);
 
 searchCity("Antwerp");
 
-// Display responses in html
-function cityWeather(response) {
-    let city = document.querySelector("#city");
-    city.innerHTML = response.data.name;
-    let date = document.querySelector("#date");
-    date.innerHTML = formatDate(response.data.dt * 1000);
-    let temperature = document.querySelector("#main-temp");
-    temperature.innerHTML = Math.round(response.data.main.temp);
-    let description = document.querySelector("#description");
-    description.innerHTML = response.data.weather[0].description;
-    let mainIcon = document.querySelector("#weather-icon");
-    mainIcon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-    mainIcon.setAttribute("alt", response.data.weather[0].description);
-    let humidity = document.querySelector("#humidity");
-    humidity.innerHTML = `Humidity: ${response.data.main.humidity} %`;
-    let wind = document.querySelector("#wind");
-    wind.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
-
-}
 
 //Format date
 
@@ -61,15 +56,44 @@ function formatHours(timestamp) {
     }
     return `${currentHour}:${currentMinutes}`;
 }
+
+// Display responses in html
+function cityWeather(response) {
+    let city = document.querySelector("#city");
+    city.innerHTML = response.data.name;
+    let date = document.querySelector("#date");
+    date.innerHTML = formatDate(response.data.dt * 1000);
+    let temperature = document.querySelector("#main-temp");
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    let description = document.querySelector("#description");
+    description.innerHTML = response.data.weather[0].description;
+    let mainIcon = document.querySelector("#weather-icon");
+    mainIcon.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    mainIcon.setAttribute("alt", response.data.weather[0].description);
+    let humidity = document.querySelector("#humidity");
+    humidity.innerHTML = `Humidity: ${response.data.main.humidity} %`;
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = `Wind: ${response.data.wind.speed} km/h`;
+
+}
+
+
 // Displaying 5 day weather forecast
 function displayFiveDaysForecast(response) {
-    let forecastData = document.querySelector("#forecast-report");
-    forecastData.innerHTML = null;
+    let forecastElement = document.querySelector("#forecast-report");
+    forecastElement.innerHTML = null;
     let forecast = null;
 
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 6; index++) {
         forecast = response.data.list[index];
-        forecastData.innerHTMl += `<div class="col-2">
+        console.log(forecast);
+        let date = new Date(forecast.dt * 1000);
+        let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+        let name = days[date.getDay()];
+        forecastElement.innerHTML += `<div class="col-2">
+              <h3>
+                ${name}
+              </h3>
               <img
                 src="http://openweathermap.org/img/wn/${
             forecast.weather[0].icon
@@ -82,19 +106,13 @@ function displayFiveDaysForecast(response) {
         )}°</strong> ${Math.round(forecast.main.temp_min)}°
               </div>
         </div>`;
-    }
+
+
+        }
+
+
 }
 
 
-// Get axios
-function searchCity(city) {
-    let apiKey = "7f2644b98089db65deba1530820c54a1";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-    axios.get(apiUrl).then(cityWeather);
-
-    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayFiveDaysForecast);
-
-}
 
 
