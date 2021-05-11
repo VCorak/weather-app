6038008cb6c064b2a9b0b9619952362d288b32af
Valentina Corak <valentinacorak@gmail.com>
2021-05-11 19:39:36 +0200

    Added current date and time

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 const input = document.getElementById('enter-city');
 const button = document.getElementById('run');
 
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+
+    let week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let currentDay = week[date.getDay()];
+    return`${currentDay} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+    let date = new Date(timestamp);
+    let currentHour = date.getHours();
+    if (currentHour < 10) {
+        currentHour = `0${currentHour}`;
+    }
+    let currentMinutes = date.getMinutes();
+    if(currentMinutes < 10 ) {
+        currentMinutes = `0${currentMinutes}`;
+    }
+
+    return `${currentHour}:${currentMinutes}`;
+}
+
 const getCity = async city => {
     const url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=21d207d4e5449385a0586090096515c7&units=metric`);
     const response = await url.json();
@@ -9,7 +39,7 @@ const getCity = async city => {
 
 
     document.getElementById("city").innerHTML = response.name;
-    //document.getElementById("date").innerHTML = formatDate(response.dt * 1000);//
+    document.getElementById("date").innerHTML = formatDate(response.dt * 1000);//
     document.getElementById("main-temp").innerHTML = Math.round(response.main.temp);
     document.getElementById("description").innerHTML = response.weather[0].description;
     document.getElementById("humidity").innerHTML = `Humidity: ${response.main.humidity} %`;
